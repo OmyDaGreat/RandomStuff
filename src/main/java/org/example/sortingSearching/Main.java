5be41e707d954826c8bee231b152311fee6ddb8c
Malefic Marauder <92651174+OmyDaGreat@users.noreply.github.com>
2024-02-10 11:15:59 -0800

    Added some comments

--- src/main/java/org/example/sortingSearching/Main.java
+++ src/main/java/org/example/sortingSearching/Main.java
@@ -7,11 +7,11 @@ public class Main {
   // Crystal Nguyen, & Yi-Ju Tseng
   public static void main(String[] args) {
     SSList s = new SSList();
-    s.addAll(Arrays.asList(1,3,5,6,7,8,2,4));
+    s.addAll(Arrays.asList(1,3,5,6,7,8,2,4)); //initialization
     s.bogoSort();
-    s.scramble();
-    System.out.println(s.recLineSearch(2));
+    s.scramble(); //unsort
+    System.out.println(s.recLineSearch(2)); //unsorted list searching
     s.recSelecSort();
-    System.out.println(s.recBinSearch(2));
+    System.out.println(s.recBinSearch(2)); //sorted list searching
   }
 }
