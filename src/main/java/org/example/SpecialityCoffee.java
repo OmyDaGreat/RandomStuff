7ff2aa2071568a8ce762bb907dba3b02ea4d53e3
Malefic Marauder <ogupta4242@gmail.com>
2024-02-21 07:24:38 +0000

    Added Unit 9: Lesson 1 so far

---
+++ src/main/java/org/example/SpecialityCoffee.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class SpecialityCoffee extends Coffee {
+    private String flavor = "";
+    
+    public SpecialityCoffee() {
+      super();
+      setFlavor("vanilla");
+    }
+    
+    public SpecialityCoffee(String size, String type, String flavor) {
+      super(size, false, 1, type);
+      setFlavor(flavor);
+    }
+    
+    public SpecialityCoffee(String size, boolean isSkinny, int shots, String type, String flavor) {
+      super(size, isSkinny, shots, type);
+      setFlavor(flavor);
+    }
+    
+    public void setFlavor(String flavor) {
+      this.flavor = flavor;
+    }
+    
+    public String getFlavor() {
+      return flavor;
+    }
+  }
