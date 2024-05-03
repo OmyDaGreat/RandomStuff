d405df9e8af52a1d00c5a601e7d16d800e179f6b
Malefic Marauder <ogupta4242@gmail.com>
2024-05-03 04:30:41 +0000

    Everything works now

--- src/main/java/org/example/yeet/WeirdNodes.java
+++ src/main/java/org/example/yeet/WeirdNodes.java
@@ -7,16 +7,16 @@ import lombok.extern.log4j.Log4j2;
 @Log4j2
 public class WeirdNodes {
     public static void main(String[] args) {
-        LinkNode one = new LinkNode(1, new LinkNode(2, new LinkNode(3, new LinkNode(4, new LinkNode(5, new LinkNode(6, new LinkNode(7, new LinkNode(8, new LinkNode(9, new LinkNode(10))))))))));
-        reorderNode(one);
+        ListNode one = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6, new ListNode(7, new ListNode(8, new ListNode(9, new ListNode(10))))))))));
+        reorderNode(one).print();
     }
-    public static LinkNode reorderNode(LinkNode head) {
-        ArrayList<LinkNode> a = new ArrayList<LinkNode>();
-        for(LinkNode node = head; node != null; node = node.next) {
+    public static ListNode reorderNode(ListNode head) {
+        ArrayList<ListNode> a = new ArrayList<ListNode>();
+        for(ListNode node = head; node != null; node = node.next) {
             a.add(node);
         }
-        ArrayList<LinkNode> firstHalf = new ArrayList<LinkNode>();
-        ArrayList<LinkNode> secondHalf = new ArrayList<LinkNode>();
+        ArrayList<ListNode> firstHalf = new ArrayList<ListNode>();
+        ArrayList<ListNode> secondHalf = new ArrayList<ListNode>();
         for(int i = 0; i < Math.ceil(a.size()/2.0);i++) {
             firstHalf.add(a.get(i));
         }
@@ -46,15 +46,15 @@ public class WeirdNodes {
     }
 }
 
-class LinkNode {
+class ListNode {
     int val;
-    LinkNode next;
+    ListNode next;
 
-    LinkNode(int val) {
+    ListNode(int val) {
         this.val = val;
         this.next = null;
     }
-    LinkNode(int val, LinkNode next) {
+    ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
     }
